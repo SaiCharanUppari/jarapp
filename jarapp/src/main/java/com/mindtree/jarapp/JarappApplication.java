150a88b272efb9d7d5163acfa9e125380e3c8744
Sai Charan Uppari <55044602+SaiCharanUppari@users.noreply.github.com>
2020-03-20 21:59:40 +0530

    Initial Commit

---
+++ jarapp/src/main/java/com/mindtree/jarapp/JarappApplication.java
@@ -0,0 +1,13 @@
+package com.mindtree.jarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarappApplication.class, args);
+	}
+
+}
---
+++ jarapp/src/test/java/com/mindtree/jarapp/JarappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.jarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
